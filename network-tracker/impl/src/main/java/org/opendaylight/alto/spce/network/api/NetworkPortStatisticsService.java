c84a84f2e231b9d2c3e72d2e9467b124601b7552
J. Austin Wang <wangjunzhuo200@gmail.com>
2015-11-07 18:00:46 +0800

    add NetworkPortStatisticsService

---
+++ network-tracker/impl/src/main/java/org/opendaylight/alto/spce/network/api/NetworkPortStatisticsService.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2015 Tongji University and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.alto.spce.network.api;
+
+public interface NetworkPortStatisticsService {
+    /**
+     * @param tpId
+     * @param metric
+     * @return current Tx speed in bps or Bps.
+     */
+    Long getCurrentTxSpeed(String tpId, Metric metric);
+
+    /**
+     * @param tpId
+     * @param metric
+     * @return current Rx speed in bps of Bps.
+     */
+    Long getCurrentRxSpeed(String tpId, Metric metric);
+
+    enum Metric {BITSPERSECOND, BYTESPERSECOND};
+}

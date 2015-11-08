b0cb1da85d9ac0e4e52df81772c0c2a21106278a
fno2010 <jingxuan.n.zhang@gmail.com>
2015-11-09 07:36:18 +0800

    Wrap getTxBandwidth.
    Signed-off-by: fno2010 <jingxuan.n.zhang@gmail.com>

--- impl/src/main/java/org/opendaylight/alto/spce/impl/algorithm/PathComputation.java
+++ impl/src/main/java/org/opendaylight/alto/spce/impl/algorithm/PathComputation.java
@@ -9,11 +9,18 @@
 package org.opendaylight.alto.spce.impl.algorithm;
 
 import org.opendaylight.yang.gen.v1.urn.opendaylight.alto.spce.rev151106.alto.spce.setup.input.ConstraintMetric;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.tracker.rev151107.AltoSpceGetTxBandwidthInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.tracker.rev151107.AltoSpceGetTxBandwidthInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.tracker.rev151107.AltoSpceGetTxBandwidthOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.tracker.rev151107.NetworkTrackerService;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TpId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yangtools.yang.common.RpcResult;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class PathComputation {
 
@@ -29,5 +36,17 @@ public class PathComputation {
 
     public List<TpId> maxBandwidthPath(TpId srcTpId, TpId dstTpId, Topology topology, List<ConstraintMetric> constraintMetrics) {
         return null;
+    }
+
+    private BigInteger getBandwidthByTp(String txTpId) {
+        BigInteger availableBandwidth = null;
+        AltoSpceGetTxBandwidthInput input = new AltoSpceGetTxBandwidthInputBuilder().setTpId(txTpId).build();
+        Future<RpcResult<AltoSpceGetTxBandwidthOutput>> result = this.networkTrackerService.altoSpceGetTxBandwidth(input);
+        try {
+            AltoSpceGetTxBandwidthOutput output = result.get().getResult();
+            availableBandwidth = output.getSpeed();
+        } catch (InterruptedException | ExecutionException e) {
+        }
+        return availableBandwidth;
     }
 }

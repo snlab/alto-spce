4201928007a9224cd020740cca5fca0d83df58ce
fno2010 <jingxuan.n.zhang@gmail.com>
2015-11-12 05:49:43 +0800

    Fix: deploy.sh
    Signed-off-by: fno2010 <jingxuan.n.zhang@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ DISTRO_DIR=$1
 
 cp features/target/alto-spce-features-1.0.0-SNAPSHOT.kar $DISTRO_DIR/deploy/
 mkdir tmp
-unzip features/target/alto-spce-features-1.0.0-SNAPSHOT.kar tmp
-cp -r tmp/repository/org DISTRO_DIR/system/
+unzip features/target/alto-spce-features-1.0.0-SNAPSHOT.kar -d tmp
+cp -r tmp/repository/org $DISTRO_DIR/system/
 rm -rf tmp

8bbfaac8afbcaa7665389b0647d70a1c3a34b227
xiangq27 <xiangq27@gmail.com>
2015-11-15 05:26:18 -0500

    start to write scheduler

--- impl/src/main/java/org/opendaylight/alto/spce/impl/scheduler/OMFRA.java
+++ impl/src/main/java/org/opendaylight/alto/spce/impl/scheduler/OMFRA.java
@@ -24,7 +24,6 @@ public class OMFRA {
 
     private boolean fileSlicingAllowed;
 
-
     public OMFRA(boolean fileSlicingAllowed) {
         this.fileSlicingAllowed = fileSlicingAllowed;
     }
@@ -33,6 +32,17 @@ public class OMFRA {
 
     public void setFileSlicingOption (boolean fileSlicingAllowed) {
         this.fileSlicingAllowed = fileSlicingAllowed;
+    }
+
+    public OMFRAAllocPolicy onlineScheduler(BandwidthTopology topology,
+                                            DataTransferRequest[] request,
+                                            int newRequestIdx,
+                                            DataTransferFlow[] flow, 
+                                            int newFlowIdx) {
+        int num_vertex = topology.getTopologySize();
+        int num_flow = flow.length;
+        int num_request = request.length;
+
     }
 
     private boolean FindResidualPath(BandwidthTopology tmpTopology,
@@ -66,7 +76,6 @@ public class OMFRA {
 
         return false;
     }
-
 
     private boolean FindPath(BandwidthTopology topology, int src, int dst) {
         boolean exitflag = false;
